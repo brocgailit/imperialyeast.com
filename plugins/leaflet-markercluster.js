a3bec008e28cce0cc317376dd77c42450ff01932
Broc Gailit <brocgailit@gmail.com>
2019-06-11 15:25:34 -0700

    Remove initial styles from markercluster

--- plugins/leaflet-markercluster.js
+++ plugins/leaflet-markercluster.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
 import Vue2LeafletMarkerCluster from 'vue2-leaflet-markercluster'
-import 'leaflet.markercluster/dist/MarkerCluster.css'
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 
 Vue.component('l-markercluster', Vue2LeafletMarkerCluster)
