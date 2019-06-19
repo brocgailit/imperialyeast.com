8b0718daed7ccfacfe4d8798f745c17e7d62350d
Broc Gailit <brocgailit@gmail.com>
2019-06-18 18:19:17 -0700

    Add head details into where to buy pages

--- pages/where-to-buy/purchase-yeast-online.vue
+++ pages/where-to-buy/purchase-yeast-online.vue
@@ -16,6 +16,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   props: {
     locations: {
@@ -26,6 +27,82 @@ export default {
   computed: {
     onlineRetailers() {
       return this.locations.filter(l => l.online_retailer)
+    },
+    ...mapState({
+      website: state => state.website
+    })
+  },
+  head() {
+    return {
+      title: `Purchase Yeast Online | ${this.website.name}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: `Find out where to get yeast online.  ${
+            this.website.name
+          } is available from online homebrew retailers.`
+        },
+        {
+          hid: 'open-graph-url',
+          property: 'og:url',
+          content: `${this.website.canonical_url}${this.$route.path}`
+        },
+        {
+          hid: 'open-graph-type',
+          property: 'og:type',
+          content: 'website'
+        },
+        {
+          hid: 'open-graph-description',
+          property: 'og:description',
+          content: `Find out where to get yeast online.  ${
+            this.website.name
+          } is available from online homebrew retailers.`
+        },
+        {
+          hid: 'open-graph-title',
+          property: 'og:title',
+          content: 'Purchase Yeast Online'
+        },
+        {
+          hid: 'open-graph-image',
+          property: 'og:image',
+          content: this.website.default_sharing_image.data.url
+        },
+        {
+          hid: 'open-graph-image-alt',
+          property: 'og:image:alt',
+          content: this.website.default_sharing_image.title
+        },
+        {
+          hid: 'twitter-card',
+          property: 'twitter:card',
+          content: 'summary_large_image'
+        },
+        {
+          hid: 'twitter-site',
+          property: 'twitter:site',
+          content: `@${this.website.twitter_handle}`
+        },
+        {
+          hid: 'twitter-description',
+          property: 'twitter:description',
+          content: `Find out where to get yeast online.  ${
+            this.website.name
+          } is available from online homebrew retailers.`
+        },
+        {
+          hid: 'twitter-description',
+          property: 'twitter:title',
+          content: 'Purchase Yeast Online'
+        },
+        {
+          hid: 'twitter-image',
+          property: 'twitter:image',
+          content: this.website.default_sharing_image.data.url
+        }
+      ]
     }
   }
 }
