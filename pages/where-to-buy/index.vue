6d744c2b4a7da609e1aa2de29edf6091419318ad
Broc Gailit <brocgailit@gmail.com>
2019-06-24 10:12:42 -0700

    Try getting geo without options (Safari test)

--- pages/where-to-buy/index.vue
+++ pages/where-to-buy/index.vue
@@ -218,13 +218,14 @@ export default {
       this.searchResults = location.hits
     }, 300),
     getGeolocation() {
-      const options = {
+      /* const options = {
         enableHighAccuracy: false,
         timeout: Infinity,
         maximumAge: 1000 * 30
-      }
+      } */
       return new Promise((resolve, reject) =>
-        navigator.geolocation.getCurrentPosition(resolve, reject, options)
+        // navigator.geolocation.getCurrentPosition(resolve, reject, options)
+        navigator.geolocation.getCurrentPosition(resolve, reject)
       )
     },
     async getLocationByGeolocation() {
