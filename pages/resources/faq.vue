ae3648e8ccdd971ca764baad24b7e926d7bc7241
Broc Gailit <brocgailit@gmail.com>
2019-06-25 17:40:05 -0700

    Initial implemenation of resources

--- pages/resources/faq.vue
+++ pages/resources/faq.vue
@@ -18,35 +18,6 @@
             </li>
           </ul>
         </article>
-        <aside class="howtos">
-          <ul class="howto-list">
-            <li v-for="(howto, idx) of howtos" :key="howto.id" class="howto">
-              <div>
-                <h4 class="howto-name">{{ howto.name }}</h4>
-                <h5 class="howto-description">
-                  {{ howto.description }}
-                </h5>
-                <ol class="howto-steps">
-                  <li
-                    v-for="(step, s) of howto.steps"
-                    :id="'howto' + howto.id + '-step' + (idx + 1)"
-                    :key="s"
-                    class="howto-step"
-                  >
-                    <p>{{ step.text }}</p>
-                    <responsive-image
-                      v-if="step.image"
-                      class="howto-step-image"
-                      :file="step.image"
-                      sizes="33vw"
-                      lazy
-                    />
-                  </li>
-                </ol>
-              </div>
-            </li>
-          </ul>
-        </aside>
       </div>
     </section>
   </div>
@@ -67,81 +38,20 @@ export default {
     })
   },
   jsonld() {
-    const schema = [
-      ...this.howtos.map(howto => {
-        const image = [...howto.steps]
-          .reverse()
-          .find(s => s.image)
-          .image.data.thumbnails.find(t => t.width > 500)
-        const h = {
-          '@context': 'http://schema.org',
-          '@type': 'HowTo',
-          name: howto.name,
-          description: howto.description,
-          image: {
-            '@type': 'ImageObject',
-            url: image.url,
-            width: image.width,
-            height: image.height
-          },
-          estimatedCost: {
-            '@type': 'MonetaryAmount',
-            currency: 'USD',
-            value: howto.estimated_cost
-          },
-          totalTime: `P${howto.total_time}M`, // TODO: figure how to make ISO strings make sense for all situations
-          step: howto.steps.map((s, i) => {
-            const step = {
-              '@type': 'HowToStep',
-              url: `${this.website.canonical_url}/faq#howto${howto.id}-step${i +
-                1}`,
-              name: s.name,
-              itemListElement: [{ '@type': 'HowToDirection', text: s.text }]
-            }
-            if (s.image) {
-              const stepImage = s.image.data.thumbnails.find(t => t.width > 500)
-              step.image = {
-                '@type': 'ImageObject',
-                url: stepImage.url,
-                width: stepImage.width,
-                height: stepImage.height
-              }
-            }
-            return step
-          })
+    const schema = {
+      '@context': 'http://schema.org',
+      '@type': 'FAQPage',
+      mainEntity: this.faq.map(q => ({
+        '@type': 'Question',
+        name: q.question,
+        dateCreated: q.created_on,
+        acceptedAnswer: {
+          '@type': 'Answer',
+          text: q.answer.replace(/(<([^>]+)>)/gi, ''),
+          dateCreated: q.modified_on
         }
-
-        if (howto.tools) {
-          h.tool = howto.tools.map(t => ({
-            '@type': 'HowToTool',
-            name: t.name
-          }))
-        }
-
-        if (howto.supplies) {
-          h.supply = howto.supplies.map(t => ({
-            '@type': 'HowToSupply',
-            name: t.name
-          }))
-        }
-
-        return h
-      }),
-      {
-        '@context': 'http://schema.org',
-        '@type': 'FAQPage',
-        mainEntity: this.faq.map(q => ({
-          '@type': 'Question',
-          name: q.question,
-          dateCreated: q.created_on,
-          acceptedAnswer: {
-            '@type': 'Answer',
-            text: q.answer.replace(/(<([^>]+)>)/gi, ''),
-            dateCreated: q.modified_on
-          }
-        }))
-      }
-    ]
+      }))
+    }
     return schema
   },
   async asyncData({ params, $axios }) {
@@ -150,15 +60,12 @@ export default {
     const faq = await $axios
       .$get(`items/frequent_questions?filter[status]=published&fields=*.*`)
       .then(res => res.data)
-    const howtos = await $axios
-      .$get(`items/how_tos?filter[status]=published&fields=*.*`)
-      .then(res => res.data)
     const page = await $axios
       .$get(
         `items/pages?single=1&filter[slug]=${slug}&fields=${fields.join(',')}`
       )
       .then(res => res.data)
-    return { page, faq, howtos }
+    return { page, faq }
   },
   head() {
     return {
@@ -239,11 +146,7 @@ export default {
 <style lang="scss">
 .faq-section {
   padding-bottom: $size-1;
-  .container {
-    display: flex;
-  }
   .faq-list {
-    flex-grow: 1;
     @include desktop {
       margin-right: $size-5;
     }
@@ -283,42 +186,6 @@ export default {
 
       .answer {
         padding: $size-7;
-      }
-    }
-  }
-
-  .howtos {
-    flex: 0 0 33%;
-
-    .howto-list {
-      padding: $size-5;
-    }
-
-    .howto-name {
-      font-weight: $weight-bold;
-      color: $grey;
-      line-height: 1;
-    }
-    .howto-description {
-      margin-top: $size-5;
-      color: $black;
-      font-size: $size-4;
-      line-height: 1.1;
-      font-weight: $weight-bold;
-    }
-
-    .howto-steps {
-      .howto-step {
-        margin-left: 1rem;
-        margin-top: $size-5;
-        .howto-step-image {
-          display: block;
-          text-align: center;
-          margin: $size-5 auto 0;
-          img {
-            width: 100%;
-          }
-        }
       }
     }
   }
---
+++ pages/resources/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="resources-home">
+    <article>
+      <nuxt-link to="/resources/faq">
+        <h3>
+          <span>Frequently Asked Questions</span>
+        </h3>
+      </nuxt-link>
+    </article>
+  </section>
+</template>
+
+<style lang="scss">
+.resources-home {
+  h3 {
+    @include brand-font;
+    font-size: $size-2;
+  }
+}
+</style>
