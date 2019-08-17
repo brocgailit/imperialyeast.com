32f60d1bf924045c058283c13e51bc38d265f59c
Broc Gailit <brocgailit@gmail.com>
2019-08-17 11:40:28 -0700

    Clear dropdown focus on click

--- components/NavbarItem.vue
+++ components/NavbarItem.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     v-if="item.value.submenus"
+    ref="item"
     aria-role="menu"
     class="navbar-item has-dropdown is-hoverable"
   >
@@ -11,7 +12,7 @@
     >
       {{ item.value.title }}
     </nuxt-link>
-    <div class="navbar-dropdown" aria-role="menuitem">
+    <div class="navbar-dropdown" aria-role="menuitem" @click="clearFocus">
       <nuxt-link
         v-for="(sub, s) of item.value.submenus"
         :key="sub.value.page._id + s"
@@ -50,6 +51,16 @@ export default {
   computed: {
     isHomePage() {
       return this.item.value.page._id === this.$store.state.website.homepage._id
+    }
+  },
+  methods: {
+    clearFocus(event) {
+      // CSS uses :focus-within to activate ...  so need to blur to remove
+      const blur = el => {
+        if (el.blur) el.blur()
+        if (el.childNodes) [...el.childNodes].forEach(blur)
+      }
+      blur(this.$refs.item)
     }
   }
 }
