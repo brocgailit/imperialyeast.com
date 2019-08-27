e2e496d5fc64d90794cff87d36c0d528c74039a1
Broc Gailit <brocgailit@gmail.com>
2019-08-27 12:58:17 -0700

    Attempt increase of concurrency (5)

--- nuxt.config.js
+++ nuxt.config.js
@@ -245,7 +245,7 @@ module.exports = {
    ** Generate options
    */
   generate: {
-    concurrency: 1,
+    concurrency: 5,
     routes: async function() {
       const baseURL = process.env.COCKPIT_URL + '/api/collections/get/'
       const strainsPath = '/organic-yeast-strains/'
