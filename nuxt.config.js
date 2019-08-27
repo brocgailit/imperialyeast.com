480ec564645cf7d9b7ee0008e4ea17e04e5fd7d7
Broc Gailit <brocgailit@gmail.com>
2019-08-27 12:35:22 -0700

    Set concurrency to 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -245,8 +245,7 @@ module.exports = {
    ** Generate options
    */
   generate: {
-    interval: 5,
-    concurrency: 20,
+    concurrency: 1,
     routes: async function() {
       const baseURL = process.env.COCKPIT_URL + '/api/collections/get/'
       const strainsPath = '/organic-yeast-strains/'
